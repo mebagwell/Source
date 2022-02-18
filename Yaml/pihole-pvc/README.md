e9901c89fded3f65c078ba45ee4ce25bb11ffa9c
Gene Bagwell <gene@mebagwell.com>
2022-02-18 09:20:55 -0600

    Remove pihole from Yaml directory, add litmus, mediawiki, and unifi

--- Yaml/pihole-pvc/README.md
+++
@@ -1,7 +0,0 @@
-Pi-Hole on Kubernetes
-
-Prerequisites:
-1. MetalLB 
-2. NFS client autoprovisioner (set as default storage class)
-
- 
--- Yaml/pihole-pvc/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-kubectl apply -f 00-namespace.yaml
-kubectl apply -f 01-pvc.yaml
-kubectl apply -f 02-deployment.yaml
-kubectl apply -f 03-service.yaml
